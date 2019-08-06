33e0a8b92720afd45a3a0794cc1e94045f875d78
tsuuuuki <38116582+tsuuuuki@users.noreply.github.com>
2019-08-06 15:15:36 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,69 +25,79 @@ class MeowDB {
      * Creates a new object in the database, if already exists it don't creates or modify anything
      * @param {string} id The ID to create and store data
      * @param {*} sValue The value to store by default in the database (default: "{}")
-     * @returns {void} Nothing
+     * @return {object} The object you just created
+     * @return {ObjectDB} If it's object it returns a ObjectDB
+     * @throws {ErrorDB}
      */
-    async create(id, sValue = {}) {
-        if (!(await Utils.checkId(id))) return Promise.reject(new ErrorDB("Invalid ID to store data"));
-        await Utils.createData(id, path.join(dir, `${this._name}.json`), sValue);
+    create(id, sValue = {}) {
+        if (!(Utils.checkId(id))) throw new ErrorDB("Invalid ID to store data");
+        var data = Utils.createData(id, path.join(dir, `${this._name}.json`), sValue);
+        if (typeof data === "object" && !(data instanceof Array)) return new ObjectDB(dir, this._name, id, sValue);
+        else return data;
     }
 
     /**
      * Get all data from database
-     * @returns {promise} All data from database
+     * @return {object} All data from database
      */
-    async all() {
-        return Promise.resolve(await Utils.readAllData(path.join(dir, `${this._name}.json`)));
+    all() {
+        return Utils.readAllData(path.join(dir, `${this._name}.json`));
     }
 
     /**
      * Get data of an element in the database
      * @param {string} id The ID of element to get
-     * @returns {promise} The information (if it's object it be a ObjectDB) or an error
+     * @return {ObjectDB} If it's object it returns a ObjectDB
+     * @return {any} The information
+     * @throws {ErrorDb}
      */
-    async get(id) {
-        if (!(await Utils.checkId(id))) return Promise.reject(new ErrorDB("Invalid ID to get data"));
-        let data = await Utils.getData(id, path.join(dir, `${this._name}.json`));
-        if (typeof data === "object" && !(data instanceof Array)) return Promise.resolve(new ObjectDB(dir, this._name, id, data));
-        else return Promise.resolve(data);
+    get(id) {
+        if (!(Utils.checkId(id))) throw new ErrorDB("Invalid ID to get data");
+        let data = Utils.getData(id, path.join(dir, `${this._name}.json`));
+        if (typeof data === "object" && !(data instanceof Array)) return new ObjectDB(dir, this._name, id, data);
+        else return data;
     }
 
     /**
      * Set data of an element in the database
      * @param {string} id The ID of element to edit
-     * @param {*} value The value to set (if not defined, the element will be undefined)
-     * @returns {promise} Only if error
+     * @param {any} value The value to set (if not defined, the element will be undefined)
+     * @return {any} The object you just modified
+     * @return {ObjectDB} If it's object it returns a ObjectDB
+     * @throws {ErrorDB}
      */
-    async set(id, value) {
-        if (!(Utils.checkId(id))) return Promise.reject(new ErrorDB("Invalid ID to set data"));
-        await Utils.setData(id, path.join(dir, `${this._name}.json`), value);
+    set(id, value) {
+        if (!(Utils.checkId(id))) throw new ErrorDB("Invalid ID to set data");
+        var data = Utils.setData(id, path.join(dir, `${this._name}.json`), value);
+        if (typeof data === "object" && !(data instanceof Array)) return new ObjectDB(dir, this._name, id, value)
+        else return data;
     }
-
     /**
      * Sort all entries from a ID in a respective element
      * @param {string} id The object ID to sort that element (* for all IDs)
      * @param {string} element The element (property) to check
      * @param {boolean} ascending If the data must be ascending (default: false)
-     * @returns {promise} An array with the data sorted or an error
+     * @return {Array} An array with the data sorted
+     * @throws {ErrorDb}
      */
-    async sort(id, element, ascending = false) {
-        if (!(await Utils.checkId(id, true))) return Promise.reject(new ErrorDB("Invalid ID to sort data"));
-        if (!(await Utils.checkName(element))) return Promise.reject(new ErrorDB("Invalid element to sort data"));
-        let data = await Utils.getData(id, path.join(dir, `${this._name}.json`), true);
-        if (typeof data !== "object") return Promise.reject(new ErrorDB("Invalid ID to sort data"));
-        if (!data[Object.keys(data)[0]][element]) return Promise.reject(new ErrorDB("Invalid element to sort data"));
-        let dataSort = Object.entries(data).sort(async (a, b) => {
+    sort(id, element, ascending = false) {
+        if (!(Utils.checkId(id, true))) throw new ErrorDB("Invalid ID to sort data");
+        if (!(Utils.checkName(element))) throw new ErrorDB("Invalid element to sort data");
+        let data = Utils.getData(id, path.join(dir, `${this._name}.json`), true);
+        if (typeof data !== "object") throw new ErrorDB("Invalid ID to sort data");
+        if (!data[Object.keys(data)[0]][element]) throw new ErrorDB("Invalid element to sort data");
+        let dataSort = Object.entries(data).sort((a, b) => {
             if (ascending) return a[1][element] - b[1][element];
             return b[1][element] - a[1][element];
         });
-        return Promise.resolve(dataSort);
+        return dataSort;
     }
 }
 
-module.exports = async (dbDir) => {
+module.exports = (dbDir) => {
     if (typeof dbDir !== "string") dbDir = require.main ? path.join(require.main.dirname, "meow-databases") : path.join(__dirname, "meow-databases");
-    if (!fs.existsSync(dbDir)) await fs.mkdirSync(dbDir);
-    if (!fs.statSync(dbDir).isDirectory()) await fs.mkdirSync(dbDir);
+    if (!fs.existsSync(dbDir)) fs.mkdirSync(dbDir);
+    if (!fs.statSync(dbDir).isDirectory()) fs.mkdirSync(dbDir);
     dir = dbDir;
     return MeowDB;
 }
