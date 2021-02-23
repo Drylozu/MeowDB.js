de4629e18e6e19783ed90662eaae1dc35b8e0885
Drylotrans <35281350+Drylotrans@users.noreply.github.com>
2021-02-23 12:05:56 -0500

    -

--- src/Utils.js
+++ src/Utils.js
@@ -48,7 +48,7 @@ class MeowDBUtils {
         if (typeof data === "object" && (data instanceof Array)) return `[${data.map((e) => this.stringifyData(e)).join(",")}]`;
         if (typeof data === "boolean") return data ? "true" : "false";
         if (typeof data === "undefined") return "undefined";
-        // if data isn't a string/number/object/boolean/undefined, it will return null to not throw any errors
+        // if data isn't a string/number/object/boolean/undefined, it will return "null" to not throw any errors
         return "undefined";
     }
 
